d7d9cd46b500f4dd621e0008c59c379e352452dd
kwonyongjun <dydwns18011@gmail.com>
2024-02-14 16:58:40 +0900

    refactor: 기능별 component 분리

---
+++ src/auth/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AuthPage = () =>{
+
+    return (
+        <div>
+            로그인 페이지
+        </div>
+    )
+}
+
+export default AuthPage;
---
+++ src/myMovie/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyMoviePage = () =>{
+
+    return(
+        <div>
+            myMoviePage
+        </div>
+    )
+}
+
+export default MyMoviePage;
