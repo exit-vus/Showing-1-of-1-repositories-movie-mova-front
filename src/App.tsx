35b05801236129a11a174ee617f11fc65fb4bcd7
kwonyongjun <dydwns18011@gmail.com>
2024-02-14 16:58:03 +0900

    feat: router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { Routes, Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import AuthPage from './auth/page';
+import MyMoviePage from './myMovie/page';
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 관리합니다.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">    
+        공통  메뉴 
       </header>
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<AuthPage/>}/>
+            <Route path="/myMovie" element={<MyMoviePage/>}/>
+          </Routes>
+        </Router>
+       
+      </main>
+      
     </div>
   );
 }
